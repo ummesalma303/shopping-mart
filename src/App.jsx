5cf7945f9155ed764932f0fa6eca6d7b9aa920d0
ummesalma303 <ummesalma333999@gmail.com>
2024-10-23 19:07:42 -0700

    added btn color

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import CartContainer from './components/CartContainer/CartContainer'
 import Header from './components/Header/Header'
 import Products from './components/Products/Products'
 
 function App() {
-  const [isActive, setActive] = useState({
-    cart: true
-  });
-  const handleToggle = (btn) => {
-    if (btn==='cart') {
-      setActive({
-        cart: true
-      })
+  // const [isActive, setActive] = useState({
+  //   cart: true
+  // });
+  // const handleToggle = (btn) => {
+  //   if (btn==='cart') {
+  //     setActive({
+  //       cart: true
+  //     })
       
-    } else {
-      setActive({
-        cart: false,
-      })
+  //   } else {
+  //     setActive({
+  //       cart: false,
+  //     })
 
-    }
-  }
+  //   }
+  // }
   
   return (
     <>
       <Header></Header>
       <div className='flex justify-around'>
       <Products></Products>
-      <CartContainer handleToggle={handleToggle} isActive={isActive}></CartContainer>
+        {/* <CartContainer handleToggle={handleToggle} isActive={isActive}></CartContainer> */}
+      <CartContainer></CartContainer>
+        
       </div>
     </>
   )
--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -1,15 +1,26 @@
 
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const CartContainer = ({ handleToggle, isActive }) => {
-    console.log(isActive);
+const CartContainer = () => {
+    const [isActive,setActive]=useState(true)
+       const handleToggle = () => {
+       
+           setActive(true);
+        
+    }
+    
     return (
         <>
          <div>
            <h1 className="text-3xl">hi i am CartContainer.jsx</h1> 
-           <div className="m-4 ">
+           {/* <div className="m-4 ">
             <button onClick={()=>handleToggle('cart')} className={`mr-4 ${isActive.cart?"btn btn-accent mr-4":'btn'}`}>Cart</button>
             <button onClick={()=>handleToggle('About')} className={`${isActive.cart?"btn ":'btn-accent'}`}>About</button>
+        </div> */}
+                <div className="m-4 ">
+            <button onClick={()=>handleToggle()} className={isActive?"btn bg-blue-400":'btn'}>Cart</button>
+            <button onClick={()=>handleToggle()} className="btn">About</button>
         </div>
         </div>
         
