fafbd0fe9faf24de4d3d43e320d94b31d66719c4
ummesalma303 <ummesalma333999@gmail.com>
2024-10-23 17:43:33 -0700

    basic setup done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Header = () => {
+    return (
+        <div>
+          <h1>Hello</h1>  
+        </div>
+    );
+};
+
+Header.propTypes = {
+    
+};
+
+export default Header;
